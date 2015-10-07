d2773099457e7b8208ab7fd1ef81306c45e8ef8c
Topi Hernandez <nokoayzack@gmail.com>
2015-10-06 22:08:51 -0500

    Promedios con Metodos y Arreglos
    Program es el archivo principal y Alumnos es la clase con la parte
    lógica del programa

---
+++ Alumnos.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promedios
+{
+    class Alumnos
+    {
+        public string nombre;
+        public float aux = 0, aux2 = 0, promGrup;
+        public int x = 0, contAprob = 0, contReprob = 0;
+
+        public void calculaPromedio(float[,] array, int alumnos, int materias)
+        {
+            for (int i = 0; i < alumnos; i++)
+            {
+                Console.Write("\nCual es el nombre del alumno {0} ", i + 1);
+                this.nombre = Console.ReadLine();
+
+                for (x = 0; x < materias; x++)
+                {
+                    Console.Write("Cual es la calificacion {0} ", x + 1);
+                    array[i, x] = float.Parse(Console.ReadLine());
+                    aux = aux + array[i, x];
+                }
+
+                for (int y = 0; y < materias; y++)
+                {
+                    array[i, materias] = (aux / x);
+                }
+                Console.WriteLine("El promedio del alumno {0} es: {1}", i + 1, array[i, materias]);
+                aux = 0;
+            }
+
+            for (int i = 0; i < alumnos; i++)
+            {
+                aux2 = aux2 + array[i, materias];
+            }
+
+            promGrup = aux2 / alumnos;
+
+            for (int i = 0; i < alumnos; i++)
+            {
+                if (array[i, materias] >= 6) contAprob++;
+                else contReprob++;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promedios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int alumnos, materias;
+
+            try
+            {
+                Console.Write("Cuantos almnos son? ");
+                alumnos = int.Parse(Console.ReadLine());
+
+                Console.Write("Cuantas materias son? ");
+                materias = int.Parse(Console.ReadLine());
+
+                float[,] calificaciones = new float[alumnos, materias + 1];
+
+                Alumnos obj = new Alumnos();
+                obj.calculaPromedio(calificaciones, alumnos, materias);
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("\nEl promedio grupal es: {0}", obj.promGrup);
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("El numero de aprobados es: {0}\nEl número de reprobados es: {1}", obj.contAprob, obj.contReprob);
+
+            }
+            catch(Exception ex)
+            {
+                Console.Beep();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("¡"+ex.Message.ToString()+"!");
+            }
+
+            Console.ReadKey();
+             
+        }
+    }
+}
