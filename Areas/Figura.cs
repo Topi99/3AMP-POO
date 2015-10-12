ea6b172bf6f9c2cd8d395bd488ccf5c008461da0
Topi Hernandez <nokoayzack@gmail.com>
2015-10-12 08:39:34 -0500

    Calculo de area de figuras
    Calculo de area de figuras

---
+++ Areas/Figura.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaTriangulos
+{
+    class Figura
+    {
+        public float CalcAreaTriang(float bas, float altura)
+        {
+            return (bas * altura) / 2;
+        }
+
+        public float CalcArea(float l)
+        {
+            return l * l;
+        }
+
+        public float CalcArea(float bas, float alt)
+        {
+            return bas * alt;
+        }
+
+        public double CalcCirc(float r)
+        {
+            return Math.PI * (Math.Pow(r, 2));
+        }
+    }
+}
---
+++ Areas/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaTriangulos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion, conti = 1;
+            float bas, altura, radio, lado;
+
+            try
+            {
+                while(conti == 1)
+                {
+                    Console.Clear();
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.Write("De que figura desea calcular su área? ");
+                    Console.WriteLine("\n1.- Triángulo\n2.- Circulo\n3.- Cuadrado\n4.- Rectángulo");
+
+                    Console.SetCursorPosition(39, 0); opcion = int.Parse(Console.ReadLine());
+                    Console.Clear();
+
+                    Figura fig = new Figura();
+
+                    switch (opcion)
+                    {
+                        case 1:
+                            Console.Write("Cual es la base: ");
+                            bas = float.Parse(Console.ReadLine());
+                            Console.Write("Cual es el altura: ");
+                            altura = float.Parse(Console.ReadLine());
+                            Console.WriteLine("El área de tu triángulo es: {0}", fig.CalcAreaTriang(bas, altura));
+                            break;
+                        case 2:
+                            Console.Write("Cuál es el radio: ");
+                            radio = float.Parse(Console.ReadLine());
+                            Console.WriteLine("El área del circulo es: {0}", fig.CalcCirc(radio));
+                            break;
+                        case 3:
+                            Console.Write("Cual es el lado: ");
+                            lado = float.Parse(Console.ReadLine());
+                            Console.WriteLine("El área del cuadrado es: {0}", fig.CalcArea(lado));
+                            break;
+                        case 4:
+                            Console.Write("Cual es la base: ");
+                            bas = float.Parse(Console.ReadLine());
+                            Console.Write("Cual es la altura: ");
+                            altura = float.Parse(Console.ReadLine());
+                            Console.WriteLine("El área del cuadrado es: {0}", fig.CalcArea(bas, altura));
+                            break;
+                    }
+
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.Write("Desea hacer otro cálculo? ");
+                    conti = int.Parse(Console.ReadLine());
+                    Console.Clear();
+                    Console.ResetColor();
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Fin del programa, presiona una tecla para continuar...");
+            Console.ReadKey();
+        }
+    }
+}
